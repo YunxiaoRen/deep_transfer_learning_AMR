16fa3cb9d17a3a8e29c133087c5eb3645fa6e87a
YunxiaoRen <34085438+YunxiaoRen@users.noreply.github.com>
2022-10-18 12:23:26 +0200

    Add files via upload

--- 02_TL.py
+++ 02_TL.py
@@ -27,6 +27,7 @@ import keras.models
 from keras.models import load_model
 from keras.models import model_from_json
 
+
 ### F1 score, precision, recall and accuracy metrics
 def recall_m(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
@@ -44,9 +45,9 @@ def f1_m(y_true, y_pred):
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 """
-==============================================================================
-2. procssing 
-==============================================================================
+=====================================================
+2. Data Pre-processing
+=====================================================
 """
 
 X_train = np.load("X_train.npy")
@@ -54,20 +55,48 @@ X_test = np.load("X_test.npy")
 y_train = np.load("y_train.npy")
 y_test = np.load("y_test.npy")
 
-X_train.shape,X_test.shape
-inputs = X_train.reshape(192, 8275,1) # change the shape based on X_train.shape
-inputs = inputs.astype('float32')
+X_train.shape,X_test.shape #((220, 8275), (55, 8275))
+inputs = X_train.reshape(192, 8275,1) ### change based on X_train shape
 targets = to_categorical(y_train)
-x_test = X_test.reshape(83, 8275,1) # change the shape based on X_test.shape
-x_test = x_test.astype('float32')
-y_test.shape
 
 """
-==============================================================================
-3. basic model training, save model weights 
-==============================================================================
+=====================================================
+3. Load Pre-trained Model and weights
+=====================================================
 """
 
+model = Sequential()
+model.add(Conv1D(filters=8, kernel_size=3, activation='relu',input_shape=(8275,1))) ### change based on X_train shape
+model.add(BatchNormalization())
+model.add(Conv1D(filters=8, kernel_size=3, padding='same',activation='relu'))
+model.add(MaxPooling1D(pool_size=(2)))
+model.add(Conv1D(filters=16, kernel_size=3,  padding='same',activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv1D(filters=16, kernel_size=3,  padding='same',activation='relu'))
+model.add(MaxPooling1D(pool_size=(2)))
+model.add(Flatten())
+
+## best model
+model.load_weights('AMR_pre_trained_model.h5',by_name=True) ### pretrained model
+### freeze layers
+model.layers[0].trainable = True
+model.layers[1].trainable = False
+model.layers[5].trainable = False
+model.layers[6].trainable = False
+layers = [(layer, layer.name, layer.trainable) for layer in model.layers]
+pd.DataFrame(layers, columns=['Layer Type', 'Layer Name', 'Layer Trainable'])
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(2, activation='softmax'))
+model.build(input_shape=[None,8275,1]) ### change based on X_train shape
+model.summary()
+
+
+"""
+=====================================================
+4. Transfer training on new dataset
+=====================================================
+"""
 batch_size = 8
 no_classes = 2
 no_epochs = 70
@@ -80,26 +109,12 @@ kfold = KFold(n_splits=num_folds, shuffle=True)
 fold_no = 1
 model_history=[]
 for train, test in kfold.split(inputs, targets):
-    model = Sequential()
-    model.add(Conv1D(filters=8, kernel_size=3, activation='relu',input_shape=(8275,1))) # change the shape based on X_train.shape
-    model.add(BatchNormalization())
-    model.add(Conv1D(filters=8, kernel_size=3, padding='same',activation='relu'))
-    model.add(MaxPooling1D(pool_size=(2)))
-    model.add(Conv1D(filters=16, kernel_size=3,  padding='same',activation='relu'))
-    model.add(BatchNormalization())
-    model.add(Conv1D(filters=16, kernel_size=3,  padding='same',activation='relu'))
-    model.add(MaxPooling1D(pool_size=(2)))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(2, activation='softmax'))
-    # Compile the model
-    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['acc',f1_m,precision_m, recall_m])
+    model.compile(loss='categorical_crossentropy',optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),metrics=['acc',f1_m,precision_m,recall_m])
     # Generate a print
     print('--------------------------------')
     print(f'Training for fold {fold_no} ...')
-    ## checkpoint for saving model weights 
-    filepath="Basic_best_{epoch:03d}-{f1:03f}-{val_f1:03f}.h5"
+    ## checkpoint for saving model
+    filepath="TL_best_{epoch:03d}-{f1:03f}-{val_f1:03f}.h5" ### save best model in each fold
     checkpoint = ModelCheckpoint(filepath, monitor='val_f1_m', verbose=1)
     # Fit data to model
     train_model = model.fit(inputs[train], targets[train],batch_size=batch_size,epochs=no_epochs,callbacks=[checkpoint],verbose=verbosity,validation_data=(inputs[test], targets[test]))
@@ -107,19 +122,13 @@ for train, test in kfold.split(inputs, targets):
     # Increase fold number
     fold_no = fold_no + 1
 
-## save model structure
+## save model
 json_string = model.to_json()
-open('Basic_CNN_architecture.json','w').write(json_string)
+open('TL_architecture.json','w').write(json_string)
 # save model history
 model_out = DataFrame(model_history)
-model_out.to_csv("Basic_model_history_out.csv",index=False)
+model_out.to_csv("TL_model_history_out.csv",index=False) ### save evaluation metrics
 
-
-"""
-=========================================================================
-3. Plot the accuracy and loss of training and validation dataset 
-=========================================================================
-"""
 ## plot
 plt.figure()
 plt.title('Train Accuracy vs Val Accuracy')
@@ -135,33 +144,19 @@ plt.plot(model_history[4]['acc'], color='blue',label=r'T_Acc Fold 5: (best = %0.
 plt.plot(model_history[4]['val_acc'], color='red',label=r'V_Acc Fold 5: (best = %0.3f)' % (np.max(model_history[4]['val_acc'])))
 #plt.ylim((0.7, 1.03))
 plt.legend(prop={'size':6}) 
-plt.savefig('Basic_acc_eva.pdf')
-plt.show()
-
-## plot
-plt.figure()
-plt.title('Train Loss vs Val Loss')
-plt.plot(model_history[0]['loss'], color='blue',label=r'T_Loss Fold 1: (best = %0.3f)' % (np.max(model_history[0]['loss'])))
-plt.plot(model_history[0]['val_loss'], color='red',label=r'V_Loss Fold 1: (best = %0.3f)' % (np.max(model_history[0]['val_loss'])))
-plt.plot(model_history[1]['loss'], color='blue',label=r'T_Loss Fold 2: (best = %0.3f)' % (np.max(model_history[1]['loss'])))
-plt.plot(model_history[1]['val_loss'], color='red',label=r'V_Loss Fold 2: (best = %0.3f)' % (np.max(model_history[1]['val_loss'])))
-plt.plot(model_history[2]['loss'], color='blue',label=r'T_Loss Fold 3: (best = %0.3f)' % (np.max(model_history[2]['loss'])))
-plt.plot(model_history[2]['val_loss'], color='red',label=r'V_Loss Fold 3: (best = %0.3f)' % (np.max(model_history[2]['val_loss'])))
-plt.plot(model_history[3]['loss'], color='blue',label=r'T_Loss Fold 4: (best = %0.3f)' % (np.max(model_history[3]['loss'])))
-plt.plot(model_history[3]['val_loss'], color='red',label=r'V_Loss Fold 4: (best = %0.3f)' % (np.max(model_history[3]['val_loss'])))
-plt.plot(model_history[4]['loss'], color='blue',label=r'T_Loss Fold 5: (best = %0.3f)' % (np.max(model_history[4]['loss'])))
-plt.plot(model_history[4]['val_loss'], color='red',label=r'V_Loss Fold 5: (best = %0.3f)' % (np.max(model_history[4]['val_loss'])))
-#plt.ylim((0.7, 1.03))
-plt.legend(prop={'size':6}) 
-plt.savefig('Basic_loss_eva.pdf')
+plt.savefig('TL_acc_eva.pdf')
 plt.show()
 
 
 """
-=========================================================================
-4. Evaluation on testing set 
-=========================================================================
+=====================================================
+5. Evaluation on testing dataset
+=====================================================
 """
+
+x_test = X_test.reshape(83, 8275,1)  ### change based on X_test shape
+x_test = x_test.astype('float32')
+y_test.shape
 
 y_pred_proba = model.predict(x_test)
 y_pred_classes = y_pred_proba.argmax(axis=-1)
@@ -169,7 +164,7 @@ f1_matrix = confusion_matrix(y_test,y_pred_classes)
 f1_report = classification_report(y_test,y_pred_classes)
 MCC = matthews_corrcoef(y_test,y_pred_classes)
 
-file = open("Basic_cnn_confusion_matrix.csv","w")
+file = open("TL_cnn_confusion_matrix.csv","w")
 file.write(str(MCC))
 file.write(str(f1_report))
 file.write(str(f1_matrix))
@@ -185,9 +180,9 @@ plt.xlabel('False Postive Rate')
 plt.ylabel('True Positive Rate')
 plt.title("ROC Curve")
 plt.legend(loc='best')
-plt.savefig("Basic_CNN_ROC_curve.pdf",bbox_inches='tight')
-np.savetxt("Basic_CNN_fpr.csv",fpr_keras,delimiter=",")
-np.savetxt("Basic_CNN_tpr.csv",tpr_keras,delimiter=",")
+plt.savefig("TL_CNN_ROC_curve.pdf",bbox_inches='tight')
+np.savetxt("TL_CNN_fpr.csv",fpr_keras,delimiter=",")
+np.savetxt("TL_CNN_tpr.csv",tpr_keras,delimiter=",")
 
 
 ###### PR curve
@@ -199,6 +194,7 @@ plt.plot(recall,precision,label= '(AUCPR = {:.3f})'.format(pr_auc))
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.legend(loc='best')
-plt.savefig("Basic_CNN_P-R_curve.pdf")
-np.savetxt("Basic_CNN_precision.csv",precision,delimiter=",")
-np.savetxt("Basic_CNN_recall.csv",recall,delimiter=",")
+plt.savefig("TL_CNN_P-R_curve.pdf")
+np.savetxt("TL_CNN_precision.csv",precision,delimiter=",")
+np.savetxt("TL_CNN_recall.csv",recall,delimiter=",")
+
